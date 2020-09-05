c462f6e2cf6bd86681ca188248c6260551900e3c
prashant-kumar-masand <prashantkumarmasand@gmail.com>
2020-09-05 22:40:54 +0530

    Home component integration

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from './services/home.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/internal/operators';
+import { CommonService } from 'src/app/core/services/common.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private ngUnsubscribe: Subject<any> = new Subject();
+  public rocketDetails: any;
+  public reqObj: any = {
+    limit: 10,
+    launch_year: null,
+    launch_success: null,
+    land_success: null
+  };
 
-  constructor() { }
+  constructor(
+    private homeService: HomeService,
+    private commonService: CommonService
+  ) {}
 
   ngOnInit(): void {
+    this.getSpaceExLaunches();
   }
 
+  private getSpaceExLaunches() {
+    this.homeService
+      .getSpaceExLaunches(this.reqObj)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+        resData => {
+          this.rocketDetails = resData;
+        },
+        err => {
+          this.commonService.errorHandler(err.error);
+        }
+      );
+  }
+
+  public updateList(obj) {
+    console.log(obj);
+    this.reqObj = { ...this.reqObj, ...obj };
+    this.getSpaceExLaunches();
+  }
+
+  public getLanding(item) {
+    console.log(item);
+
+    if (
+      item &&
+      item.rocket &&
+      item.rocket.first_stage &&
+      item.rocket.first_stage.cores instanceof Array
+    )
+      if (item.rocket.first_stage.cores[0]) {
+        return item.rocket.first_stage.cores[0].land_success;
+      } else {
+        return null;
+      }
+  }
+
+  /**
+   * @function ngOnDestroy
+   * @description unsubscribed from observables and hide the loader
+   */
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }
---
+++ src/app/modules/home/services/home.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpParams,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { catchError, map, retry } from 'rxjs/internal/operators';
+import { throwError, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+  private apiBaseUrl: string = '';
+
+  constructor(private http: HttpClient) {
+    this.apiBaseUrl = `${environment.apiBaseUrl}/${environment.apiVersion}/${environment.apiBasePath}`;
+  }
+
+  getSpaceExLaunches(queryData) {
+    const options = {
+      params: this.buidlQueryParams(queryData)
+    };
+    return this.http.get<any>(this.apiBaseUrl, options).pipe(
+      retry(0),
+      map(res => {
+        return res;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(error);
+      })
+    );
+  }
+
+  buidlQueryParams(query) {
+    let params = new HttpParams();
+    if (query.launch_success)
+      params = params.append('launch_success', query.launch_success);
+    if (query.land_success)
+      params = params.append('land_success', query.land_success);
+    if (query.limit) params = params.append('limit', query.limit);
+    if (query.launch_year)
+      params = params.append('launch_year', query.launch_year);
+    return params;
+  }
+}
