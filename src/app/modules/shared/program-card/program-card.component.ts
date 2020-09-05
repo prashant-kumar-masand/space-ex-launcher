bbf7d8275469e91d5dd0e22a98212394f6fa20c3
prashant-kumar-masand <prashantkumarmasand@gmail.com>
2020-09-05 22:39:42 +0530

    program-card integration

--- src/app/modules/shared/program-card/program-card.component.ts
+++ src/app/modules/shared/program-card/program-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-program-card',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./program-card.component.scss']
 })
 export class ProgramCardComponent implements OnInit {
+  @Input()
+  public flightNumber: number;
+  @Input()
+  public missionName: string;
+  @Input()
+  public rocketImageLink: string;
+  @Input()
+  public missionIds: Array<any>;
+  @Input()
+  public launchYear: string;
+  @Input()
+  public successfulLaunch: boolean;
+  @Input()
+  public successfulLanding: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
