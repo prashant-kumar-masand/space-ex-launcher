3d6b14e76df0da5c47d4e4f80ebb018e7fe8bfb1
prashant-kumar-masand <prashantkumarmasand@gmail.com>
2020-09-06 09:56:09 +0530

    Testing : Unit test cases footer spec

--- src/app/modules/shared/footer/footer.component.spec.ts
+++ src/app/modules/shared/footer/footer.component.spec.ts
@@ -1,25 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { DOMHelper } from 'src/testing/dom-helper';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let domHelper: DOMHelper<FooterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
+      declarations: [FooterComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
+    domHelper = new DOMHelper(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have text inside footer tag as 'Developed by : Prashant Kumar Singh'`, () => {
+    expect(domHelper.getText('footer')).toBe(
+      'Developed by : Prashant Kumar Singh'
+    );
   });
 });
