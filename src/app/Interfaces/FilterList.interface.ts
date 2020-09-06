2d8b320d31fe5e0fc321ad85459de178da1033eb
prashant-kumar-masand <prashantkumarmasand@gmail.com>
2020-09-06 13:32:46 +0530

    Interfaces : created interfaces for filter-bar and Space rocket

---
+++ src/app/Interfaces/FilterList.interface.ts
@@ -0,0 +1,14 @@
+export interface YearList {
+  name: string;
+  value: number;
+}
+
+export interface LaunchList {
+  name: string;
+  value: string;
+}
+
+export interface LandList {
+  name: string;
+  value: string;
+}
---
+++ src/app/Interfaces/FilterOptions.interface.ts
@@ -0,0 +1,6 @@
+export interface FilterOptions {
+  limit: number;
+  launch_year?: null | number;
+  launch_success?: null | string;
+  land_success?: null | string;
+}
