3c2f077a95b35adc7fe3534efa4b4c9a9bac9fc1
prashant-kumar-masand <prashantkumarmasand@gmail.com>
2020-09-09 06:43:19 +0530

    Canonical Service undo changes not impacting seo

--- src/app/core/services/canonical.service.ts
+++ src/app/core/services/canonical.service.ts
@@ -9,17 +9,9 @@ export class CanonicalService {
 
   public setCanonicalURL(url?: string) {
     const canURL = url == undefined ? this.dom.URL : url;
-    /** canonical link for Mobile version **/
-    const linkMobile: HTMLLinkElement = this.dom.createElement('link');
-    linkMobile.setAttribute('rel', 'canonical');
-    this.dom.head.appendChild(linkMobile);
-    linkMobile.setAttribute('href', canURL);
-    linkMobile.setAttribute('media', 'only screen and (min-width: 700px)');
-    /** canonical link for Desktop version **/
-    const linkDesktop: HTMLLinkElement = this.dom.createElement('link');
-    linkDesktop.setAttribute('rel', 'alternate');
-    this.dom.head.appendChild(linkDesktop);
-    linkDesktop.setAttribute('href', canURL);
-    linkDesktop.setAttribute('media', 'only screen and (max-width: 700px)');
+    const link: HTMLLinkElement = this.dom.createElement('link');
+    link.setAttribute('rel', 'canonical');
+    this.dom.head.appendChild(link);
+    link.setAttribute('href', canURL);
   }
 }
